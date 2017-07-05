eb90246e0e9c87f111f5f30e8324b82c236b02f2
shen <shenxuhui1992@163.com>
2017-07-05 15:11:57 +0800

    update

--- test-azure-iot/device/main.py
+++ test-azure-iot/device/main.py
@@ -2,3 +2,7 @@
 import os
 
 os.system('python ./iothub_client_sample.py -c "HostName=IOT-nostalgia.azure-devices.cn;DeviceId=sample;SharedAccessKey=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" -p mqtti')
+
+#os.system('python ./iothub_client_sample_class.py -c "HostName=IOT-nostalgia.azure-devices.cn;DeviceId=sample;SharedAccessKey=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" -p mqtti')
+
+#os.system('python ./iothub_client_sample_x509.py -c "HostName=IOT-nostalgia.azure-devices.cn;DeviceId=sample;SharedAccessKey=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" -p mqtti')
